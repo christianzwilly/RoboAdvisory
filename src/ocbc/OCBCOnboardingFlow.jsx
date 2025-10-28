7ec603b746aa8b0b6132ead1af77027521384b82
christianzwilly <139091557+christianzwilly@users.noreply.github.com>
2025-10-28 16:53:56 +0700

    refactor: convert OCBC onboarding flow to pure JSX and add propTypes

--- src/ocbc/OCBCOnboardingFlow.jsx
+++ src/ocbc/OCBCOnboardingFlow.jsx
@@ -1,3 +1,13 @@
+import React, { useMemo, useState } from "react";
+import PropTypes from "prop-types";
+// include all your existing imports here
+
+export default function OCBCOnboardingFlow() {
+  // paste the converted JSX component body here
+}
+
+// No props used yet
+OCBCOnboardingFlow.propTypes = {};
 import React, { useMemo, useState } from 'react'
 
 // NOTE: This is your uploaded OCBC-themed onboarding flow, lightly adapted to run in Vite + Tailwind.
